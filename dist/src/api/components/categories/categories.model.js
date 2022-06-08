960ee76a8105b2a524abe3a5ea0c58168c17e200
alex <alex.adaumi@gmail.com>
2022-06-08 17:58:28 -0300

    :fix: remove dist folder

--- dist/src/api/components/categories/categories.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CategoriesModel = void 0;
-const mongoose_1 = require("mongoose");
-const categories_schema_1 = require("./categories.schema");
-const CategoriesModel = (0, mongoose_1.model)("Categories", categories_schema_1.CategoriesSchema);
-exports.CategoriesModel = CategoriesModel;
