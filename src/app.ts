78039ebc294589576d3ef60e0138c7167f5a27fc
alex <alex.adaumi@gmail.com>
2022-06-08 18:11:16 -0300

    fix: procfile

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import "dotenv/config";
+require("dotenv").config();
 import { Server } from "./api/server";
 
 const PORT = process.env.PORT || 80;
 const server = new Server();
 
 server.listen(PORT);
+
