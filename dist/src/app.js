7713d09d8daba900661d28d16f10906d23aa734d
alex <alex.adaumi@gmail.com>
2022-06-08 17:28:56 -0300

    fix: :hammer: remove dist folder

--- dist/src/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const server_1 = require("./api/server");
-const PORT = process.env.PORT || 80;
-const server = new server_1.Server();
-server.listen(PORT);
